f7e6b5434795f3917425c48de2a9b40fcf31b81c
Cleidson Oliveira <cleidsonoliveira00@gmail.com>
2022-11-11 15:31:54 -0300

    chore: add navigation types

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,15 @@
+export declare global {
+    namespace ReactNavigation {
+        interface RootParamList {
+            home: undefined;
+            stats: undefined;
+            newmeal: undefined;
+            feedback: undefined;
+            meal: undefined;
+
+            players: {
+                group: string;
+            }
+        }
+    }
+}
